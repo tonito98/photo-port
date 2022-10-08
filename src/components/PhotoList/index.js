1b1a94cbfdf8821dae90430fdef6884042248f0a
tonito98 <86890715+tonito98@users.noreply.github.com>
2022-10-08 18:30:17 +0200

    modal opens as expected when an image from the gallery is clicked

--- src/components/PhotoList/index.js
+++ src/components/PhotoList/index.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-// import Modal from '../Modal';
+import Modal from '../Modal';
 
 
 
@@ -104,15 +104,23 @@ const PhotoList = ( {category} ) => {
     ]);
     // console.log(currentCategory.category);
     const currentPhotos = photos.filter((photo) => photo.category === category);
-
+    const [isModalOpen, setIsModalOpen] = useState(false);
+    const [currentPhoto, setCurrentPhoto] = useState();
+    const toggleModal = (image, i) => {
+        // current photo
+        setCurrentPhoto({...image, index: i})
+        setIsModalOpen(true);
+    }
     return (
        <div> 
+        {isModalOpen && <Modal currentPhoto={currentPhoto}/>}
         <div className="flex-row">
           {currentPhotos.map((image, i) => (
             <img
               src={require(`../../assets/small/${category}/${i}.jpg`)}
               alt={image.name}
               className='img-thumbnail mx-1'
+              onClick={() => toggleModal(image, i)}
               key={image.name}
             />
           ))}  
